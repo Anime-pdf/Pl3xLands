16d87c6e6faa1bf3f0654fbce2ed41d685cf1bd0
Paul <paul.turkovskiy@gmail.com>
2026-02-10 20:58:39 +0200

    some storage enhancements

--- src/main/kotlin/me/animepdf/pl3xLands/storage/JsonFileStorage.kt
+++ src/main/kotlin/me/animepdf/pl3xLands/storage/JsonFileStorage.kt
@@ -1,22 +1,42 @@
 package me.animepdf.pl3xLands.storage
 
 import com.google.gson.Gson
+import me.animepdf.pl3xLands.dto.Region
 import me.animepdf.pl3xLands.dto.RegionManifest
 import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 
 class JsonFileStorage(private val file: File, private val gson: Gson) : RegionStorage {
-    override fun save(manifest: RegionManifest) {
+    var manifest: RegionManifest? = null
+
+    override fun save(manifest: RegionManifest?) {
+        if (manifest != null) this.manifest = manifest
+        if(this.manifest == null) return
         FileWriter(file).use { writer ->
-            gson.toJson(manifest, writer)
+            gson.toJson(this.manifest, writer)
         }
     }
 
-    override fun load(): RegionManifest? {
+    override fun load(fresh: Boolean): RegionManifest? {
         if (!file.exists()) return null
+        if (!fresh && manifest != null) return manifest
         return FileReader(file).use { reader ->
             gson.fromJson(reader, RegionManifest::class.java)
         }
+    }
+
+    override fun addRegions(regions: Array<Region>, save: Boolean) {
+        if (manifest == null) load()
+        manifest?.regions += regions
+        if (save && manifest != null)
+            save(manifest!!)
+    }
+
+    override fun addChunksToRegion(regionId: String, chunks: Array<Long>, save: Boolean) {
+        if (manifest == null) load()
+        manifest?.regions?.find { it.id == regionId }?.chunks += chunks
+        if (save && manifest != null)
+            save(manifest!!)
     }
 }
--- src/main/kotlin/me/animepdf/pl3xLands/storage/RegionStorage.kt
+++ src/main/kotlin/me/animepdf/pl3xLands/storage/RegionStorage.kt
@@ -1,8 +1,11 @@
 package me.animepdf.pl3xLands.storage
 
+import me.animepdf.pl3xLands.dto.Region
 import me.animepdf.pl3xLands.dto.RegionManifest
 
 interface RegionStorage {
-    fun save(manifest: RegionManifest)
-    fun load(): RegionManifest?
+    fun save(manifest: RegionManifest? = null)
+    fun load(fresh: Boolean = false): RegionManifest?
+    fun addRegions(regions: Array<Region>, save: Boolean = true)
+    fun addChunksToRegion(regionId: String, chunks: Array<Long>, save: Boolean = true)
 }
