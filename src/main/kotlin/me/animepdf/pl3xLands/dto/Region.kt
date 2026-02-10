6d44b58bfb9d9a6803b56419660213ade4905e73
Paul <paul.turkovskiy@gmail.com>
2026-02-10 21:00:40 +0200

    store mutable lists

--- src/main/kotlin/me/animepdf/pl3xLands/dto/Region.kt
+++ src/main/kotlin/me/animepdf/pl3xLands/dto/Region.kt
@@ -7,33 +7,5 @@ data class Region(
     val owner: String,
     val contact: String,
     val world: String,
-    val chunks: LongArray
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Region
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (description != other.description) return false
-        if (owner != other.owner) return false
-        if (contact != other.contact) return false
-        if (world != other.world) return false
-        if (!chunks.contentEquals(other.chunks)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + description.hashCode()
-        result = 31 * result + owner.hashCode()
-        result = 31 * result + contact.hashCode()
-        result = 31 * result + world.hashCode()
-        result = 31 * result + chunks.contentHashCode()
-        return result
-    }
-}
+    val chunks: MutableList<Long>
+)
--- src/main/kotlin/me/animepdf/pl3xLands/dto/RegionManifest.kt
+++ src/main/kotlin/me/animepdf/pl3xLands/dto/RegionManifest.kt
@@ -3,25 +3,5 @@ package me.animepdf.pl3xLands.dto
 data class RegionManifest(
     val hash: String,
     val timestamp: Long,
-    val regions: Array<Region>
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as RegionManifest
-
-        if (timestamp != other.timestamp) return false
-        if (hash != other.hash) return false
-        if (!regions.contentEquals(other.regions)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = timestamp.hashCode()
-        result = 31 * result + hash.hashCode()
-        result = 31 * result + regions.contentHashCode()
-        return result
-    }
-}
+    val regions: MutableList<Region>
+)
